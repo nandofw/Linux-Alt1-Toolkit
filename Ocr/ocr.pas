476426c9e0f3ef1164b17815060e6f8415620130
nandofw <fernandofeiden@gmail.com>
2018-08-11 23:41:09 -0400

    Add files via upload

--- Ocr/ocr.pas
+++ Ocr/ocr.pas
@@ -18,12 +18,17 @@ end;
 ocrfont = record
 name : string;
 size : integer;
+maxdif:integer;
+espace: integer;
+shadow:boolean;
 font : array of tocrfont;
 end;
 Tarrayinteger = array of integer;
 
-function Readline(fontname: string; rgb,posx,posy:integer;mybit:TPicture):String;
-Procedure addfont(fontname, filename:string; size:integer);
+//zfunction Readline2(fontname: string; rgb,posx,posy:integer;mybit:TPicture):String;
+function Readline(fontname: string; r,g,b:integer; bmp:TBGRAbitmap):String;  // used in xptracker
+Procedure addfont(fontname, filename:string;size,maxdif,espace:integer;shadow:boolean);
+//function getnextline(bmp:TBGRABitmap; x,r,g,b:integer):integer;
 
 var
 font : array of ocrfont;
@@ -58,7 +63,7 @@ begin
  result := buff1;
 end;
 
-Procedure addfont(fontname,filename:string; size:integer);
+Procedure addfont(fontname, filename:string;size,maxdif,espace:integer;shadow:boolean);
 var
 StrGrid: TStringGrid;
 x,z,zz:integer;
@@ -67,6 +72,9 @@ begin
  setlength(font, z+1);
  font[z].name:= fontname;
  font[z].size:=size;
+ font[z].maxdif:=maxdif;
+ font[z].espace:=espace;
+ font[z].shadow:=shadow;
  Strgrid := TStringGrid.create(nil);
  strgrid.LoadFromCSVFile(filename,',');
  for x:=1 to strgrid.RowCount-1 do
@@ -80,19 +88,30 @@ begin
  strgrid.Free;
 end;
 
-function bestchar(x1,y1:integer; buff1: Tarrayinteger; bmp:TBGRABitmap;r,g,b:integer):integer;
+function bestchar(x1,y1:integer; buff1: Tarrayinteger; bmp:TBGRABitmap;r,g,b:integer; shadow:boolean):integer;
 var
-diff,tt,b1: integer;
+diff,x,tt,n,b1,b2: integer;
+aux:string;
 Line: tBGRAPixel;
 begin
+n:=0;
 tt:=0;
 diff:=0;
-b1:= length(buff1)-1;
+b1:= length(buff1);
 while (tt < b1) do
 begin
  Line  :=bmp.GetPixel(buff1[tt]+y1,buff1[tt+1]+x1);
  diff := diff+ abs(buff1[tt+2]-(abs(line.red-r)+abs(line.blue-b)+abs(line.green-g)));
+ if shadow then
+ begin
+ line := bmp.GetPixel(buff1[tt]+y1+1,buff1[tt+1]+x1+1);
+ diff := diff+ abs(buff1[tt+3]-(abs(line.red-r)+abs(line.blue-b)+abs(line.green-g)));
+ tt:=tt+4;
+ end
+ else
+ begin
  tt:=tt+3;
+ end;
 end;
 result := diff;
 end;
@@ -125,24 +144,18 @@ end;
  result := m;
 end;
 
-function Readline(fontname: string; rgb,posx,posy:integer;mybit:TPicture):String;
+
+function Readline(fontname: string; r,g,b:integer; bmp:TBGRAbitmap):String;
 var
-fontid,z,x,x1,x2,y1,y2,pixelcount,lasty,n,nf,cc,best,ss,tt,cwt:integer;
-r,g,b,m:integer;
-found,first:boolean;
+fontid,z,x,x1,x2,x3,y1,y2,pixelcount,lasty,n,nf,cc,best,ss,tt,maxdif,size,esp,m:integer;
+found,first,shadow:boolean;
 datastr:string;
 line : tBGRAPixel;
 datast: array of string;
 datass: array of integer;
-bmp : TBGRAbitmap;
 begin
-b:= rgb and 255;
-g:= rgb>>8 and 255;
-r:= rgb>>16 and 255;
 lasty:=0;
 datastr:='';
-bmp:= TBGRABitmap.Create(380,20);
-bmp.Bitmap.Canvas.CopyRect(rect(0,0,380,20),Mybit.Bitmap.canvas, rect(posx-200,posy-10,posx+380-200,posy+20-10));
 z := -1;
  for z:=0 to length(font) -1 do
  begin
@@ -151,47 +164,55 @@ z := -1;
  end ;
  if not(z = -1 ) then
  begin
- cwt:= font[fontid].size;
+ m := getnextline(bmp,0,r,g,b);
  z:=0;
- m:=getnextline(bmp,0,r,g,b);
  first := false;
- x1 := m+cwt;
- x2 := m ;
- while z < bmp.Width-1 do
-  begin
-  found:=false;
-  for x := m-1 to m+cwt do
-           begin
-           Line  :=bmp.GetPixel(z,x);
-           if ((abs(line.red -r)+abs(line.green-g)+abs(line.blue-b))< 100)then
-              begin
-              found := true;
-              if x1 > x then
-              x1:= x;
-              if x2 < x then
-              x2 := x;
-              end;
-           end;
-  if (found) and not(first) then
-  begin
-  first := true;
-  y1:= z;
+ size:= font[fontid].size;
+ esp:= font[fontid].espace;
+ shadow := font[fontid].shadow;
+ maxdif := font[fontid].maxdif;
+x1 := m+size;
+x2 := m ;
+while z < bmp.Width-1 do
+ begin
+ found:=false;
+ for x := m-1 to m+12 do
+          begin
+          Line  :=bmp.GetPixel(z,x);
+          if ((abs(line.red -r)+abs(line.green-g)+abs(line.blue-b))< maxdif)then
+             begin
+             found := true;
+             if x1 > x then
+             x1:= x;
+             if x2 < x then
+             x2 := x;
+             end;
+          end;
+ if (found) and not(first) then
+ begin
+ first := true;
+ y1:= z;
   end ;
-  if (not(found) and first) then
+ if (not(found) and first) then
+ begin
+ y2 := z;
+ inc(x2);
+ first := false;
+   //-------------------------
+  pixelcount := ((x2-x1))*((y2-y1));
+  if not(datastr = '')then
   begin
-  y2:=z;
-  first := false;
-  pixelcount:= (y2-y1)*(x2-x1);
-  if (abs(lasty-y1) > 3) and not(datastr ='') then
+  if (abs(lasty-y1) > esp) then
   datastr:= datastr+' ';
+  end;
   lasty:=y2 ;
   n:=0;
   for nf:=0 to length(font[fontid].font)-1 do
            begin
            if pixelcount = font[fontid].font[nf].pixelcount then
            begin
-           cc := bestchar(x1,y1,font[fontid].font[nf].pixels, bmp,r,g,b);
-           if (cc < 300) then
+           cc := bestchar(x1,y1,font[fontid].font[nf].pixels, bmp,r,g,b,shadow);
+           if (cc < 250) then
            begin
            setlength(datast,n+1);
            setlength(datass,n+1);
@@ -203,7 +224,7 @@ z := -1;
            end;
   if not(n=0) then
   begin
-  best:=300;
+  best:=500;
   ss:=-1;
   for tt:= 0 to n-1 do
            begin
@@ -219,14 +240,12 @@ z := -1;
   end;
   end;
   first:=false;
-  x1 := m+cwt;
-  x2 := m ;
+   x1 := m+size;
+   x2 := m ;
   end;
   inc(z)
   end;
-  end;
- //bmp.SaveToFile('./teste/ocr.bmp');  //debug
- bmp.Free;
+ end;
  result := datastr;
 end;
 
